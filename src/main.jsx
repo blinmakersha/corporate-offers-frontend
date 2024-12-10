9fb5e002d4061c4ca97f4de843dde43d1f2c02d5
Linapush <linapush2002@gmail.com>
2024-12-10 19:56:08 +0300

    routing depending on authentication;

--- src/main.jsx
+++ src/main.jsx
@@ -8,10 +8,21 @@ import "./index.css";
 import HomePage from "./pages/HomePage/HomePage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 
+const isAuthenticated = () => {
+  return localStorage.getItem("isAuthenticated") === "true";
+};
+
+const ProtectedRoute = ({ children }) => {
+  return isAuthenticated() ? children : <Navigate to="/login" replace />;
+};
+
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <HomePage />,
+    element: 
+      <ProtectedRoute>
+        <HomePage />
+      </ProtectedRoute>
   },
   {
     path: "/login",
@@ -19,6 +30,7 @@ const router = createBrowserRouter([
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />
 );
