ece631b382ae325c58afe20cb0a0444d291976ca
Valentina Kulgavykh <valentinakulg@gmail.com>
2024-12-12 16:38:57 +0300

    feat: HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,29 @@
-import React from "react";
-import OfferCard from "../../components/OfferCard/OfferCard";
+import React, { useState } from "react";
+import OfferCard from "../../components/OfferCard/OfferCard.jsx";
+import Filters from "../../components/Filters/Filters.jsx";
 import "./HomePage.css";
+import { testOfferCards } from "../../utils/testData";
 
 const HomePage = () => {
+  const [offerCards, setOfferCards] = useState(testOfferCards);
+
   return (
-    <div className="wrap-offer-card">
-      <OfferCard />
-      <OfferCard />
-      <OfferCard />
+    <div className="home-page">
+      <h3 className="home-page__about-user">Вы авторизованы как Сотрудник</h3>
+      <h1 className="home-page__main-title">Корпоративные скидки</h1>
+      <div className="home-page__info-buttons">
+        <Filters/>
+        <a href="" className="home-page__guide">
+          Как воспользоваться скидкой?
+        </a>
+      </div>
+      <div className="home-page__wrap-offer-card">
+        {offerCards.map((item) => (
+          <span key={item.id}>
+            <OfferCard data={item} />
+          </span>
+        ))}
+      </div>
     </div>
   );
 };
