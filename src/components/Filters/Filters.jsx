89fe4828f797bb6ac69e9a193143a9959eedd946
Valentina Kulgavykh <valentinakulg@gmail.com>
2024-12-12 16:37:50 +0300

    feat: Filter component

---
+++ src/components/Filters/Filters.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Filters.css';
+import filterIcon from "../../assets/filterIcon.svg"
+
+const Filters = () => {
+
+    return (
+    <button className="filters-button">
+        Фильтры
+    </button>
+    )
+  };
+  
+  export default Filters;
