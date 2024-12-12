5150b3e51da9b8fc49c5898db772ae09e971c37a
Valentina Kulgavykh <valentinakulg@gmail.com>
2024-12-12 16:37:30 +0300

    fix: OfferCard component

--- src/components/OfferCard/OfferCard.jsx
+++ src/components/OfferCard/OfferCard.jsx
@@ -1,29 +1,30 @@
-import React from "react";
+import React from "react"
 import './OfferCard.css'
 import Chip from '../Chip/Chip'
 
-const OfferCard = () => {
+const OfferCard = (props) => {
+  const { data } = props;
+
   return <div className="offer-card">
     <div className="offer-card__image">
-      <img src="" alt="img" />
-      <Chip kind="profit">ВЫГОДА</Chip>
-      <Chip kind="sale">-30%</Chip>
+      <img src={data.image} alt="img" />
+      <Chip className="offer-card__sale" kind={data?.sale ? "sale" : "profit"}>{data?.sale ? data.sale : 'ВЫГОДА'}</Chip>
     </div>
     <div className="offer-card__info">
-      <h2 class="offer-card__title">Название компании</h2>
-      <p>Описание компании</p>
+      <h2 className="offer-card__title">{data.title}</h2>
+      <p className="offer-card__description">{data.description}</p>
     </div>
     <div className="offer-card__tags">
       <div className="offer-card__tags_wrap">
         <div className="offer-card__tags_location">
-          <Chip kind="city">Город, местонахождение</Chip>
-          <Chip className="offer-card__amount" kind="num">+3</Chip>
+          <Chip kind="city">{data.city}</Chip>
+          <Chip className="offer-card__amount" kind="num">{data.ammount}</Chip>
         </div>
-        <p className="offer-card__tag">#Тег</p>
+        <p className="offer-card__tag">#{data.tag}</p>
       </div>
-      <p className="offer-card__deadline">До..</p>
+      <p className="offer-card__deadline">До {data.date}</p>
     </div>
-  </div>;
+  </div>
 };
 
 export default OfferCard;
---
+++ src/utils/testData.js
@@ -0,0 +1,34 @@
+export const testOfferCards = [
+    {
+        id: 1,
+        image: 'https://img.freepik.com/free-photo/sports-tools_53876-138077.jpg',
+        sale: '-30%',
+        title: 'Название компании',
+        description: 'Описание компании',
+        city: 'Город, местонахождение',
+        ammount: '+3',
+        tag: 'Спорт',
+        date: '11.12.2024',
+    },
+    {
+        id: 1,
+        image: 'https://img.freepik.com/free-photo/sports-tools_53876-138077.jpg',
+        title: 'Название компании',
+        description: 'Описание компании',
+        city: 'Город, местонахождение',
+        ammount: '+3',
+        tag: 'Спорт',
+        date: '11.12.2024',
+    },
+    {
+        id: 1,
+        image: 'https://img.freepik.com/free-photo/sports-tools_53876-138077.jpg',
+        sale: '-15%',
+        title: 'Название компании',
+        description: 'Описание компании',
+        city: 'Город, местонахождение',
+        ammount: '+3',
+        tag: 'Спорт',
+        date: '11.12.2024',
+    }
+]
