99b11352550f3e368ff4f08d11c181af14d4b5e6
Linapush <linapush2002@gmail.com>
2024-12-13 10:35:54 +0300

    update ShowMore button;

--- src/components/ShowMore/ShowMore.jsx
+++ src/components/ShowMore/ShowMore.jsx
@@ -2,16 +2,34 @@ import { useState } from 'react';
 
 const ShowMoreButton = ({ loadMoreItems }) => {
     const [itemsCount, setItemsCount] = useState(10);
+    const MAX_ITEMS = 50;
+    const [message, setMessage] = useState('');
 
     const handleClick = () => {
-        setItemsCount((prevCount) => prevCount + 10);
-        loadMoreItems(itemsCount);
+        if (itemsCount >= MAX_ITEMS) {
+            setMessage('Вы достигли максимального количества элементов для загрузки');
+            return;
+        }
+
+        try {
+            const newCount = itemsCount + 10;
+            loadMoreItems(newCount);
+            setItemsCount(newCount);
+            setMessage(`Загружено ${newCount} элементов.`);
+        }
+        catch(error) {
+            console.error('Ошибка при загрузке элементов:', error);
+            setMessage('Произошла ошибка при загрузке элементов. Попробуйте снова позже.');
+        }
     };
 
     return (
-        <button onClick={handleClick}>
-            Показать еще
-        </button>
+        <div>
+            <button onClick={handleClick}>
+                Показать еще
+            </button>
+            {message && <p>{message}</p>}
+        </div>
     );
 };
 
