9295a608c1061c727cefb2446c528b268edef2ac
Valentina Kulgavykh <valentinakulg@gmail.com>
2024-12-15 23:29:52 +0300

    feat: Accordion component

---
+++ src/components/Accordion/Accordion.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useRef, useState } from "react";
+
+import "./Accordion.css";
+
+function Accordion(props) {
+  const [active, setActive] = useState(false);
+  const content = useRef(null);
+  const [height, setHeight] = useState("0px");
+
+  useEffect(() => {
+    console.log("Height for ", props.title, ": ", height);
+  }, [height]);
+
+  function toggleAccordion() {
+    setActive(!active);
+    setHeight(active ? "0px" : `${content.current.scrollHeight}px`);
+  }
+
+  return (
+    <div className="accordion__section">
+      <div
+        className={`accordion ${active ? "active" : ""}`}
+        onClick={toggleAccordion}
+      >
+        <span style={{ marginRight: "10px" }}>
+          <svg
+            width="21"
+            height="21"
+            viewBox="0 0 21 21"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M5.25 7.875L10.5 13.125L15.75 7.875" stroke="#222222" />
+          </svg>
+        </span>
+        <p className="accordion__title">{props.title}</p>
+      </div>
+      <div
+        ref={content}
+        style={{ maxHeight: `${height}` }}
+        className="accordion__content"
+      >
+        <div
+          className="accordion__text"
+          dangerouslySetInnerHTML={{ __html: props.content }}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Accordion;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,22 +1,36 @@
 import React, { useState } from "react";
 import OfferCard from "../../components/OfferCard/OfferCard.jsx";
 import Filters from "../../components/Filters/Filters.jsx";
+import Accordion from "../../components/Accordion/Accordion.jsx";
 import "./HomePage.css";
 import { testOfferCards } from "../../utils/testData";
 
 const HomePage = () => {
   const [offerCards, setOfferCards] = useState(testOfferCards);
+  const [openFilter, setOpenFilter] = useState(false);
 
   return (
     <div className="home-page">
       <h3 className="home-page__about-user">Вы авторизованы как Сотрудник</h3>
       <h1 className="home-page__main-title">Корпоративные скидки</h1>
       <div className="home-page__info-buttons">
-        <Filters/>
+        <Filters openFilter={openFilter} setOpenFilter={setOpenFilter} />
         <a href="" className="home-page__guide">
           Как воспользоваться скидкой?
         </a>
       </div>
+      {openFilter && (
+        <div className="home-page__filters">
+          <Accordion
+            title="Все города"
+            content="<a>Город 1</a><a>Город 2</a><a>Город 3</a>"
+          />
+          <Accordion
+            title="Все категории"
+            content="<a>Категория 1</a><a>Категория 2</a><a>Категория 3</a>"
+          />
+        </div>
+      )}
       <div className="home-page__wrap-offer-card">
         {offerCards.map((item) => (
           <span key={item.id}>
