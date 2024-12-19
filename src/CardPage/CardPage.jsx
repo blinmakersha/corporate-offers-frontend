06d57586dfcdf221e16405928229ca5f27dfe411
Linapush <linapush2002@gmail.com>
2024-12-19 12:22:36 +0300

    init CardPage;

---
+++ src/CardPage/CardPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import OfferCard from "../../components/OfferCard/OfferCard";
+import "./CardPage.css";
+
+const CardPage = () => {
+  return (
+    <div className="">
+      <OfferCard />
+    </div>
+  );
+};
+
+export default CardPage;
