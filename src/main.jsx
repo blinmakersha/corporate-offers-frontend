6ba109b838ae2aa41da9989dfde80bd619c81396
Linapush <linapush2002@gmail.com>
2024-12-20 13:49:12 +0300

    update routing for cardpage;

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import "./index.css";
 import HomePage from "./pages/HomePage/HomePage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import AdminPage from "./pages/AdminPage/AdminPage";
+import CardPage from "./pages/CardPage/CardPage.jsx";
 import CreateOfferCard from "./pages/CreateOfferCardPage/CreateOfferCardPage.jsx";
 import Layout from "../src/components/Layout/Layout.jsx";
 
@@ -32,9 +33,21 @@ const router = createBrowserRouter([
     element: <LoginPage />,
   },
   {
+    path: "/cardpage",
+    element: <CardPage />,
+  },
+  {
     path: "create-offer-car",
     element: <CreateOfferCard />,
   },
+  {  
+    path: "/card/:id",
+    element: (
+      <ProtectedRoute>
+        <CardPage />
+      </ProtectedRoute>
+    ),
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
