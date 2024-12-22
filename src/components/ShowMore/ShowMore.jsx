febaf571204a571644de8c81361cbbf562877bfa
Linapush <linapush2002@gmail.com>
2024-12-22 20:58:46 +0300

    show more button's rollback;

--- src/components/ShowMore/ShowMore.jsx
+++ src/components/ShowMore/ShowMore.jsx
@@ -1,36 +1,13 @@
-import { useState } from 'react';
+import "./ShowMore.css";
 
-const ShowMoreButton = ({ loadMoreItems }) => {
-    const [itemsCount, setItemsCount] = useState(10);
-    const MAX_ITEMS = 50;
-    const [message, setMessage] = useState('');
-
-    const handleClick = () => {
-        if (itemsCount >= MAX_ITEMS) {
-            setMessage('Вы достигли максимального количества элементов для загрузки');
-            return;
-        }
-
-        try {
-            const newCount = itemsCount + 10;
-            loadMoreItems(newCount);
-            setItemsCount(newCount);
-            setMessage(`Загружено ${newCount} элементов.`);
-        }
-        catch(error) {
-            console.error('Ошибка при загрузке элементов:', error);
-            setMessage('Произошла ошибка при загрузке элементов. Попробуйте снова позже.');
-        }
-    };
+const ShowMore = (props) => {
+    const { setPage } = props;
 
     return (
-        <div>
-            <button onClick={handleClick}>
-                Показать еще
-            </button>
-            {message && <p>{message}</p>}
-        </div>
+        <a className="show-more" onClick={() => setPage((prev) => prev + 50)}>
+        Показать еще...
+        </a>
     );
 };
 
-export default ShowMoreButton;
+export default ShowMore;
